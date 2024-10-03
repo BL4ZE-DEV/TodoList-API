aebb832c61988dd8f17aab0828bddf37567bd14b
BL4ZE-DEV <ayedunsultan1@gmail.com>
2024-10-03 14:10:30 +0100

    working on frontend

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::post('/register', [AuthenticationController::class, 'register']);
+Route::post('/register', [AuthenticationController::class, 'register'])->name('register');
 Route::post('/login', [AuthenticationController::class, 'login']);
 
 Route::middleware('auth:api')->group(function(){
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/register', function() {
+    return view('register');
+});
+
+
+Route::get('/login', function ()  {
+   return view('login'); 
+});
+
+Route::get('todo', function() {
+    return view('todo');
+});
